d058867ee163be280063f13ddb76c3e0d7069506
Ubuntu <ubuntu@ip-10-224-39-241.us-west-2.compute.internal>
2013-07-18 02:24:45 +0000

    bootstrapped

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -9,7 +10,7 @@ app.get('/', function(request, response) {
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
